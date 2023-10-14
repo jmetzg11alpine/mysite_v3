b27f2f92377ec23f1438b73d9b632b123c85e4bf
jmetzg11alpine <jmetzg11@gmail.com>
2023-10-14 17:56:04 -0500

    work summary filled in

--- src/components/summary/ProjectItem.js
+++ src/components/summary/ProjectItem.js
@@ -20,35 +20,26 @@ const ProjectItem = ({ project }) => {
     listItemStyle.paddingBottom = "10px";
   }
 
-  const iconStyle = {
-    cursor: "pointer",
-    margin: "0 .5rem",
-  };
-
-  const ulStyle = {
-    borderBottom: "1px solid #ccc",
-    marginBottom: "0",
-  };
-
-  const liStyle = {
-    marginLeft: "1.5rem",
-  };
   return (
     <div>
       <div style={listItemStyle}>
-        <div style={iconStyle} onClick={toggleExpansion}>
+        <div className="summary-icon" onClick={toggleExpansion}>
           {isExpanded ? <FaMinus /> : <FaPlus />}
         </div>
         <div>{project.title}</div>
       </div>
       {isExpanded && (
-        <ul style={ulStyle}>
-          {project.details.map((detail, index) => (
-            <li style={liStyle} key={index}>
-              {detail}
-            </li>
-          ))}
-        </ul>
+        <div className="summary-expanded-container">
+          <ul>
+            {project.details.map((detail, index) => (
+              <li key={index}>{detail}</li>
+            ))}
+          </ul>
+          <div>
+            <b>Result: </b>
+            {project.result}
+          </div>
+        </div>
       )}
     </div>
   );
--- src/components/summary/Summary.js
+++ src/components/summary/Summary.js
@@ -7,9 +7,10 @@ const Summary = () => {
       title: "03/2023 Onboarding and starting dashboard with D3.js",
       details: [
         "Learning about the Logistic Industry",
-        "Becoming familiar with the core applicatioin built with Django Rest API and React",
+        "Becoming familiar with the core applicatioin built with Django Rest framework and React",
         "Started a dashboard to show key KPIs with D3.js",
       ],
+      result: "Became with logistics industry and new company",
     },
     {
       id: 2,
@@ -21,25 +22,32 @@ const Summary = () => {
         "Added a time filter and terminal filter",
         "Add tooltip for hover",
       ],
+      result:
+        "User can get a quick understand of status through visual representations",
     },
     {
       id: 3,
       title: "05/2023 Pricing Model and Accounting",
       details: [
         "Trained ML model for price predicting. Key features where start, end, skids, weight, vehicle type, and duration. Used CatBoost from Yandex",
-        "Prepared financial data to move from Quickbooks to Sage",
-        "Added new field to of 'scheduled pickup' and incorporated it to various parts of the application",
+        "Added new field of 'scheduled pickup' and incorporated it to various parts of the application",
+        "Created new input fields on various forms to insure data consistency",
       ],
+      result:
+        "Users can now automatically get range for appropriate prices for new orders. The scheduled pickup time saves users from a manual calculation. Data is now more reliable.",
     },
     {
       id: 4,
       title: "06/2023 Started Warehouse application",
       details: [
-        "Started a tool that warehouse associates can track order with",
-        "Meeting to find pain points for warehouse associates",
-        "Built a UI that shows incoming and outgoing order with the ability to mark loaded or unloaded",
+        "A dashboard made with D3.js current orders for each warehouse",
+        "A new UI that shows incoming and outgoing order by warehouse",
+        "User can mark orders as loaded or unloaded",
+        "Added a notes field where operators can leave special messages",
         "Added to new fields to the database to reflect new functionality",
       ],
+      result:
+        "There is more transparency between warehouse associates and operators. Operators can give accucurate and real time updates to customers",
     },
     {
       id: 5,
@@ -48,32 +56,46 @@ const Summary = () => {
         "Orders are now grouped by bus and can be expanded to see individual orders",
         "User can now take photos of orders",
         "User can now print labels for the orders",
-        "User can now scan order QR codes with iPad",
+        "User can now scan order QR codes with an iPad",
         "Three new data tables added and created file storage management stystem on the server to store the files",
+        "Functionality can be done on the bus level to quicken work flow",
       ],
+      result:
+        "Processes that used to be done on various tools are done with one tool which improved efficiency and reliability",
     },
     {
       id: 6,
       title: "08/2023 Dashboard for sales teams and user history",
       details: [
-        "some details",
-        "some other details",
-        "some other details that take up a lot more space",
+        "Automated data gathering from Insightly",
+        "Data displayed in a custom format to reveal KPIs to management",
+        "Every action that changes data related to an order is now recorded to a new data table that shows who, when and what was changed to an order",
       ],
+      result:
+        "Management can now clearly see KPIs related to sales in one place. More transparency and accountability for data changes related to orders.",
     },
     {
       id: 7,
-      title: "09/2023 arcGIS map",
+      title: "09/2023 arcGIS map and warehouse applicaiton improvements",
       details: [
-        "some details",
-        "some other details",
-        "some other details that take up a lot more space",
+        "Through Google Maps api, the longitude and latitude of every pickup and delivery was recorded in a new data table",
+        "Through arcGIS every active order is displayed on a map",
+        "In the UI of the warehouse application certain columns and rows are highlight different colors",
       ],
+      result:
+        "User now have a geographic view of current orders. Users can quickly identify pressing issues on the warehouse applicaitons due to color highlights",
     },
     {
       id: 8,
-      title: "10/2023 something",
-      details: ["something cools something cools"],
+      title: "10/2023 Improvments on map and worked on bus application",
+      details: [
+        "Users can now make custome routes on the map",
+        "Tooptip added to the map to show order numbers, duration, dirver information, and date and times",
+        "The form on the bus sheet has stricter inputs",
+      ],
+      result:
+        "User can now use the map to compare potential new orders with current orders. Data is now more reliable in the bus application \
+      and user can fill in the form quicker",
     },
   ];
   return <ProjectList projects={projects} />;
